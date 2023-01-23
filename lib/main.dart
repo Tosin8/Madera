ac44d056f0edcb6c4578b1bc1735d2e43e30dbe1
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-23 21:28:38 +0100

    Create file for class contact_book

--- lib/main.dart
+++ lib/main.dart
@@ -21,37 +21,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Contact {
-  // Creating Contact Class.
-  final String name;
-  const Contact({required this.name}); //constructor.
-}
 
-class ContactBook {
-  // Creating Contact Book.
-  ContactBook._sharedInstance(); // private constructor
-  static final ContactBook _shared = ContactBook._sharedInstance();
-  factory ContactBook() => _shared;
-
-  // Creating Contact Storage, using the list because of using a ListView.builder.
-  final List<Contact> _contacts = [];
-
-  int get length => _contacts.length; // exposing how many contacts to have.
-
-// Simple Add Function on Contact Book
-  void add({required Contact contact}) {
-    _contacts.add(contact);
-  }
-
-  // Simple Remove Function on Contact Book
-  void remove({required Contact contact}) {
-    _contacts.remove(contact);
-  }
-
-  // A function to retrieve contacts with index
-  Contact? contact({required int atIndex}) =>
-      _contacts.length > atIndex ? _contacts[atIndex] : null;
-}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});

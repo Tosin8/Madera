a45ac086667a3516739031eabf0755de2eb07a35
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-23 21:10:30 +0100

    Updating with Simple Add and Remove Function

--- lib/main.dart
+++ lib/main.dart
@@ -21,15 +21,30 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Contact { // Creating Contact Class. 
+class Contact {
+  // Creating Contact Class.
   final String name;
   const Contact({required this.name}); //constructor.
 }
 
-class ContactBook{ // Creating Contact Book. 
+class ContactBook {
+  // Creating Contact Book.
+  ContactBook._sharedInstance(); // private constructor
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
 
+  // Creating Contact Storage
+  final List<Contact> _contacts = [];
+
+  int get length => _contacts.length; // exposing how many contacts to have.
+
+// Simple Add Function on Contact Book
+  void add({required Contact contact}) {
+    _contacts.add(contact);
+  }
 }
 
+// Simple Remove Function on Contact Book
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 

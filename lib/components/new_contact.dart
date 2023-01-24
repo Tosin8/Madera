313c1d79cddde1cb47f40d85e5d94b7d8ff8c034
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-24 09:48:02 +0100

    Create file for New Contact

---
+++ lib/components/new_contact.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart'; 
+
+class NewContactView extends StatefulWidget {
+  const NewContactView({super.key});
+
+  @override
+  State<NewContactView> createState() => _NewContactViewState();
+}
+
+class _NewContactViewState extends State<NewContactView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(title: ), 
+    );
+  }
+}

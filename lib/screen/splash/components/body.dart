8381296f11698e404e6182f646286470727b6d60
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-30 05:30:47 +0100

    Updating Splash Screen

--- lib/screen/splash/components/body.dart
+++ lib/screen/splash/components/body.dart
@@ -10,6 +10,31 @@ class SplashBody extends StatefulWidget {
 class _SplashBodyState extends State<SplashBody> {
   @override
   Widget build(BuildContext context) {
-    return ();
+    return SafeArea(
+        child: SizedBox(
+            width: double.infinity,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/splash/1.jpg'),
+                          fit: BoxFit.cover)),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.bottomRight,
+                            stops: const [
+                          0.2,
+                          0.9
+                        ],
+                            colors: [
+                          Colors.black.withOpacity(.6),
+                          Colors.black.withOpacity(.1)
+                        ])),
+                  ),
+                )
+              ],
+            )));
   }
 }
